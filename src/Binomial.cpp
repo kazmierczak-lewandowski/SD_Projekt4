19bfb9451c7c62707c78fe589027b51a023bc12c
kazik <github.scarcity013@passmail.net>
2025-06-22 15:33:24 +0200

    Private functions to work on Binomial

--- src/Binomial.cpp
+++ src/Binomial.cpp
@@ -1,1 +1,36 @@
 #include "Binomial.hpp"
+std::unique_ptr<Binomial::Node> Binomial::mergeTrees(
+    std::unique_ptr<Node>& t1, std::unique_ptr<Node>& t2) {
+  if (t1->value < t2->value) {
+    mergeTrees(t2, t1);
+  }
+  t2->parent = t1.get();
+  t2->sibling = std::move(t1->child);
+  t1->child = std::move(t2);
+  t1->degree++;
+  return std::move(t1);
+}
+void Binomial::consolidate() {
+  std::vector<std::unique_ptr<Node>> trees(64, nullptr);
+  std::unique_ptr<Node> current = std::move(head);
+  head = nullptr;
+
+  while (current != nullptr) {
+    std::unique_ptr<Node> next = std::move(current->sibling);
+    int degree = current->degree;
+
+    while (trees[degree] != nullptr) {
+      current = mergeTrees(current, trees[degree]);
+      degree++;
+      trees[degree] = nullptr;
+    }
+    trees[degree] = std::move(current);
+    current = std::move(next);
+  }
+  for (auto& tree : trees) {
+    if (tree != nullptr) {
+      tree->sibling = std::move(head);
+      head = std::move(tree);
+    }
+  }
+}
--- src/Binomial.hpp
+++ src/Binomial.hpp
@@ -1,14 +1,41 @@
 #ifndef BINOMIAL_HPP
 #define BINOMIAL_HPP
+#include <memory>
+
 #include "Heap.hpp"
 
 class Binomial : public Heap {
+private:
+  struct Node {
+    Element value;
+    Node* parent;
+    std::unique_ptr<Node> child;
+    std::unique_ptr<Node> sibling;
+    int degree;
+
+    explicit Node(const Element val) {
+      value = val;
+      parent = nullptr;
+      child = nullptr;
+      sibling = nullptr;
+      degree = 0;
+    }
+  };
+  std::unique_ptr<Node> head;
+
+  static std::unique_ptr<Node> mergeTrees(std::unique_ptr<Node>& t1,
+                                              std::unique_ptr<Node>& t2);
+  void consolidate();
  public:
+  explicit Binomial() {
+    head = nullptr;
+  }
   [[nodiscard]] Element peek() const override;
   Element extractMax() override;
   void increaseKey(const Element& element, int newPriority) override;
   void insert(Element element) override;
   void meld(Heap& otherHeap) override;
+
 };
 
 
